2eece80274d2bb131484390f022e996b29c6ea48
Chien-Chia Huang <chienchiahuang38@gmail.com>
2023-03-13 21:04:51 +0100

    commit after 2 years

--- main.cpp
+++ main.cpp
@@ -9,5 +9,7 @@ int main(){
     std::cout << "The sum of 30 and 20 is equal: " << adder(30,20) << std::endl;
     std::cout << "The difference of 30 and 20 is equal: " << substracter(30,20) << std::endl;
 
+    std::cout << "First commit after 2 years" << ;
+
     return 0;
 }
